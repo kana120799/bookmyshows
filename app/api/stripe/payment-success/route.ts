914881127b59f7367615bb9c58ec8099dbb1220f
pc <kanasharma120799@gmail.com>
2025-03-25 19:48:12 +0530

    payment type error solve

--- app/api/stripe/payment-success/route.ts
+++
@@ -1,8 +0,0 @@
-// import { handlePaymentCompletion } from "@/controllers/bookingController";
-// import { NextRequest } from "next/server";
-
-// export async function POST(request: NextRequest) {
-//   const { bookingId, paymentId, paymentIntentId } = await request.json();
-//   console.log("dnfjkdfjksd423432", bookingId, paymentId, paymentIntentId);
-//   return handlePaymentCompletion({ bookingId, paymentId, paymentIntentId });
-// }
