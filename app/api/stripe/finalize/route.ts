a211d854a2b5bc4a00af4417103552aef6f39b88
pc <kanasharma120799@gmail.com>
2025-03-28 17:21:54 +0530

    profile UI correction:3

--- app/api/stripe/finalize/route.ts
+++ app/api/stripe/finalize/route.ts
@@ -1,16 +1,17 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { handleError } from "@/middleware/errorHandler";
+import { NextRequest, NextResponse } from "next/server";
+import { handleError } from "@/middleware/errorHandler";
 // import { handlePaymentSuccess } from "@/controllers/bookingControllercopy";
 
-// export const POST = handleError(async (req: NextRequest) => {
-//   const { paymentIntentId, bookingId } = await req.json();
+export const POST = handleError(async (req: NextRequest) => {
+  const { paymentIntentId, bookingId } = await req.json();
 
-//   if (!paymentIntentId || !bookingId) {
-//     return NextResponse.json(
-//       { error: "Missing paymentIntentId or bookingId" },
-//       { status: 400 }
-//     );
-//   }
+  if (!paymentIntentId || !bookingId) {
+    return NextResponse.json(
+      { error: "Missing paymentIntentId or bookingId" },
+      { status: 400 }
+    );
+  }
+  return NextResponse.json({ message: "random" }, { status: 200 });
 
-//   return await handlePaymentSuccess({ paymentIntentId, bookingId });
-// });
+  // return await handlePaymentSuccess({ paymentIntentId, bookingId });
+});
