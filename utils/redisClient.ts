ffa2d000297a92ba6d74d351a17b74cab32d3be1
pc <kanasharma120799@gmail.com>
2025-04-17 00:12:28 +0530

    remove cron job:1

--- utils/redisClient.ts
+++ utils/redisClient.ts
@@ -1,9 +1,10 @@
 import Redis from "ioredis";
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST || "localhost",
-  port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASSWORD || undefined,
+  // host: process.env.REDIS_HOST || "localhost",
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD,
   retryStrategy(times) {
     const delay = Math.min(times * 50, 2000);
     return delay;
